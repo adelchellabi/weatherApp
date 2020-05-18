07939cf5c2c43f131986201ba963ea0155d39137
adelchellabi <adimino96@gmail.com>
2020-05-18 01:33:56 +0100

    Updates

---
+++ precache-manifest.4121f5da8fd3f193251c76d0fa38413f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b456521472369972284d5774fd2c121",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "ba65af49f5b9db6046b9",
+    "url": "/weatherApp/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "c84730eadd41a4346f00",
+    "url": "/weatherApp/static/css/main.2f3eb9d0.chunk.css"
+  },
+  {
+    "revision": "ba65af49f5b9db6046b9",
+    "url": "/weatherApp/static/js/2.6639bf75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherApp/static/js/2.6639bf75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c84730eadd41a4346f00",
+    "url": "/weatherApp/static/js/main.d7182d7d.chunk.js"
+  },
+  {
+    "revision": "d808d008bb8f9c392b0f",
+    "url": "/weatherApp/static/js/runtime-main.9dc0af7a.js"
+  }
+]);
